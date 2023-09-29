00e8bb1657354b9ded35faffdab57808723e180e
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-29 11:10:05 +0100

    Refactor Meme component & build app

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,8 +3,8 @@ export default function Meme() {
         <main className="meme">
             <section>
                 <form action="#" className="meme--form">
-                    <input type="text" className="meme--input" aria-label="" placeholder="Shut up" />
-                    <input type="text" className="meme--input" aria-label="" placeholder="and take my money" />
+                    <input type="text" className="meme--input" aria-label="top-text" placeholder="Top text" />
+                    <input type="text" className="meme--input" aria-label="bottom-text" placeholder="Bottom text" />
                     <button className="meme-btn">Get a new meme image 🖼️</button>
                 </form>
             </section>
