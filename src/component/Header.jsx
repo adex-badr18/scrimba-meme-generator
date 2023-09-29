eee743ce88da5da3dd2cf4cfc388c5177940f217
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-29 09:33:53 +0100

    Style Header component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
         <header>
             <nav>
                 <img src={logo} alt="Troll face logo" className="header--logo" />
-                <h3 className="header--logo-text">Meme Geberator</h3>
-                <h5 className="header--course-title">React Course - Project 3</h5>
+                <h3 className="header--logo-text">Meme Generator</h3>
+                <h4 className="header--course-title">React Course - Project 3</h4>
             </nav>
         </header>
     )
