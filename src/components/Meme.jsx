179f6da1b85cb6c349a5be077e36884330f85f8f
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-09-30 10:57:11 +0100

    Render meme image on page with useState hook

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,23 @@
 import memesData from "../memesData";
+import { useState } from "react";
 
 export default function Meme() {
-    let imageUrl;
+    /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+    const [imageUrl, setImageUrl] = useState('');
 
-    function getImageUrl() {
+    function getMemeImage() {
         const memesArray = memesData.data.memes; 
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        imageUrl = memesArray[randomNumber].url;
-        console.log(imageUrl);
+        setImageUrl(memesArray[randomNumber].url);
     }
 
     return (
@@ -16,7 +26,10 @@ export default function Meme() {
                 <div action="#" className="meme--form">
                     <input type="text" className="meme--input" aria-label="top-text" placeholder="Top text" />
                     <input type="text" className="meme--input" aria-label="bottom-text" placeholder="Bottom text" />
-                    <button className="meme-btn" onClick={getImageUrl}>Get a new meme image 🖼️</button>
+                    <button className="meme-btn" onClick={getMemeImage}>Get a new meme image 🖼️</button>
+                </div>
+                <div className="meme--image-container">
+                    <img src={imageUrl} alt="" className="meme--image" />
                 </div>
             </section>
         </main>
