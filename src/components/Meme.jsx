f1438487d1f746ea6f957f896b668f0070e51cc5
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-03 01:05:28 +0100

    Add text to meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,8 +2,11 @@ import memesData from "../memesData";
 import { useState } from "react";
 
 export default function Meme() {
-    const [meme, setMeme] = useState({topText: '', bottomText: '', randomImage: 'https://i.imgflip.com/22bdq6.jpg'});
-
+    const [meme, setMeme] = useState({
+        topText: 'O ye who believe!', 
+        bottomText: 'Fear Allah and be dutiful to Him', 
+        randomImage: 'http://i.imgflip.com/1bij.jpg'
+    });
     const [allMemeImages, setAllMemeImages] = useState(memesData);
 
     function getMemeImage() {
@@ -18,16 +21,47 @@ export default function Meme() {
         ))
     }
 
+    function inputChange(e) {
+        const {name, value} = e.target;
+
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <main className="meme">
             <section>
                 <div action="#" className="meme--form">
-                    <input type="text" className="meme--input" aria-label="top-text" placeholder="Top text" />
-                    <input type="text" className="meme--input" aria-label="bottom-text" placeholder="Bottom text" />
+                    <input 
+                        type="text" 
+                        className="meme--input" 
+                        aria-label="top-text" 
+                        placeholder="Top text"
+                        name="topText"
+                        value={meme.topText}
+                        onChange={inputChange}     
+                    />
+
+                    <input 
+                        type="text" 
+                        className="meme--input" 
+                        aria-label="bottom-text" 
+                        placeholder="Bottom text"
+                        name="bottomText"
+                        value={meme.bottomText}
+                        onChange={inputChange}     
+                    />
+
                     <button className="meme-btn" onClick={getMemeImage}>Get a new meme image 🖼️</button>
                 </div>
                 <div className="meme--image-container">
                     <img src={meme.randomImage} alt="" className="meme--image" />
+                    <h2 className="meme--image-text top">{meme.topText}</h2>
+                    <h2 className="meme--image-text bottom">{meme.bottomText}</h2>
                 </div>
             </section>
         </main>
